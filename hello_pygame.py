0e2a2f494653462fbc702aa528fa91586440dbb9
Mark Lozsadi <mark.lozsadi@gmail.com>
2025-04-20 17:33:59 +0200

    Add basic footballfield

---
+++ hello_pygame.py
@@ -0,0 +1,32 @@
+import pygame
+
+def main():
+    # Initialize Pygame
+    pygame.init()
+    
+    # Create window: width × height
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Mark's Football game!")
+    
+    footballField = pygame.image.load("images/footballField.png").convert()
+    footballField = pygame.transform.scale(footballField, (800,600))
+    
+    screen.blit(footballField, (0,0))
+
+    # Main loop control flag
+    running = True
+    while running:
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        # Update the display
+        pygame.display.flip()
+    
+    # Quit Pygame
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
