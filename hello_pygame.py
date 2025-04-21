f7679f58e44b64a2407be38f5e75e80bc4863686
Mark Lozsadi <mark.lozsadi@gmail.com>
2025-04-21 11:43:14 +0200

    Ronaldo added

--- hello_pygame.py
+++ hello_pygame.py
@@ -10,7 +10,9 @@ def main():
     
     footballField = pygame.image.load("images/footballField.png").convert()
     footballField = pygame.transform.scale(footballField, (800,600))
-    
+    p1 = pygame.image.load("images/Ronaldo.png").convert_alpha()
+    p1 = pygame.transform.scale(p1, (80,80))
+
     screen.blit(footballField, (0,0))
 
     # Main loop control flag
@@ -21,6 +23,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
+        screen.blit(p1, (100,100))
+
         # Update the display
         pygame.display.flip()
     
