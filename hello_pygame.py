b3dcf06a4036ad8dc4363f384de2a2840d041405
Mark Lozsadi <mark.lozsadi@gmail.com>
2025-04-21 13:10:28 +0200

    Simple movement 1

--- hello_pygame.py
+++ hello_pygame.py
@@ -12,8 +12,17 @@ def main():
     footballField = pygame.transform.scale(footballField, (800,600))
     p1 = pygame.image.load("images/Ronaldo.png").convert_alpha()
     p1 = pygame.transform.scale(p1, (80,80))
-
-    screen.blit(footballField, (0,0))
+    p2 = pygame.image.load("images/Messi.png").convert_alpha()
+    p2 = pygame.transform.scale(p2,(80,80))
+   
+    p1_x = 100
+    p1_y = 100
+    p2_x = 400
+    p2_y = 400
+    p1_cx = p1.get_rect().center[0]
+    p1_cy = p1.get_rect().center[1]
+    p2_cx = p2.get_rect().center[0]
+    p2_cy = p2.get_rect().center[1]
 
     # Main loop control flag
     running = True
@@ -22,14 +31,41 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    p1_x += 10
+                elif event.key == pygame.K_LEFT:
+                    p1_x -= 10
+                elif event.key == pygame.K_UP:
+                    p1_y -= 10
+                elif event.key == pygame.K_DOWN:
+                    p1_y += 10
         
-        screen.blit(p1, (100,100))
+        
+        screen.blit(footballField, (0,0))  
+        p1_center = ((p1_x + p1_cx), (p1_y + p1_cy))
+        update_sprite(footballField, p1, p1_center, 45, )
+        footballField.blit(p2, (p2_x, p2_y))    
+
 
         # Update the display
         pygame.display.flip()
     
     # Quit Pygame
     pygame.quit()
+
+
+def update_sprite(target_surf, image_surf, center_pos, angle):
+    """
+    Rotate image_surf by angle about its center, then blit it so that
+    its center is at center_pos on target_surf.
+    """
+    # Rotate the image.
+    rotated_image = pygame.transform.rotate(image_surf, angle)
+    # Build a rect with the same center as where we want it on screen.
+    new_rect = rotated_image.get_rect(center=center_pos)
+    # Blit to screen.
+    target_surf.blit(rotated_image, new_rect)
 
 
 if __name__ == "__main__":
