00ea59eb28e1ab07bf3de2ba61cac0cf15c7ec1e
Mark Lozsadi <mark.lozsadi@gmail.com>
2025-04-24 19:24:02 +0200

    Player movement redesigned to smoother

--- hello_pygame.py
+++ hello_pygame.py
@@ -25,18 +25,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    p1.rect.x += p1.speed
-                elif event.key == pygame.K_LEFT:
-                    p1.rect.x -= p1.speed
-                elif event.key == pygame.K_UP:
-                    p1.rect.y -= p1.speed
-                elif event.key == pygame.K_DOWN:
-                    p1.rect.y += p1.speed
-        
-        
-        dt = clock.tick(15) / 1000.0 # to get seconds
+            
+        dt = clock.tick(30) / 1000.0 # to get seconds
         sprites.clear(screen, footballField)
         sprites.update(dt)
         sprites.draw(screen)
@@ -48,18 +38,6 @@ def main():
     pygame.quit()
 
 
-def update_sprite(target_surf, image_surf, center_pos, angle):
-    """
-    Rotate image_surf by angle about its center, then blit it so that
-    its center is at center_pos on target_surf.
-    """
-    # Rotate the image.
-    rotated_image = pygame.transform.rotate(image_surf, angle)
-    # Build a rect with the same center as where we want it on screen.
-    new_rect = rotated_image.get_rect(center=center_pos)
-    # Blit to screen.
-    target_surf.blit(rotated_image, new_rect)
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, image_path, pos, resize=(0,0)):
         super().__init__()  # initialize the base Sprite
@@ -70,14 +48,21 @@ class Player(pygame.sprite.Sprite):
         # create a rect for positioning
         self.rect = self.image.get_rect(center=pos)
         self.angle = 0
-        self.speed = 4
+        self.speed = 150
 
     def update(self, dt):
-        # called once per frame; dt is the time since last frame
-        # e.g. move right at 100 pixels/sec:
-        #self.rect.x += 10 * dt
-        pass
+        movement = pygame.Vector2(0, 0)
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_LEFT]):  movement.x -= 1
+        if (keys[pygame.K_RIGHT]): movement.x += 1
+        if (keys[pygame.K_UP]):    movement.y -= 1
+        if (keys[pygame.K_DOWN]):  movement.y += 1
 
+        # normalize diagonal movement
+        if movement.x != 0 and movement.y != 0:
+            movement = movement.normalize()
+        
+        self.rect.move_ip(movement * self.speed * dt)
 
 if __name__ == "__main__":
     main()
